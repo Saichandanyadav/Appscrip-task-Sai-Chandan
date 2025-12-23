fd14a5859dfb8b9583ebc700a2aa052d6db5dd05
Sai Chandan <Saichandhanyadav2002@gmail.com>
2025-12-23 14:26:56 +0530

    fix: Updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index',
+        permanent: true,
+      },
+    ];
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['lucide-react'], // ensures SSR components work on Vercel
+  },
 };
 
 export default nextConfig;
